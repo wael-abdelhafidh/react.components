9628a69ffc099e3ff2e90503257485233f43dece
waelan <94851835+waelan@users.noreply.github.com>
2022-03-06 23:37:19 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import Address from './Component/Profile/Address';
+import FullName from './Component/Profile/FullName';
+import ProfilePhoto from './Component/Profile/ProfilePhoto';
+import './style.css'
+
+
+
+function App() {
+  
+  return (
+    <div>
+
+      <ProfilePhoto/>
+      <FullName/>
+      <Address/>
+    
+    </div>
+  );
+}
+
+export default App;
